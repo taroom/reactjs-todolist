81a02af0cef85410a2bae4efc2f2380ed6ad8184
Sultan Room <sultanroomcode@gmail.com>
2024-05-19 18:49:44 +0700

    initial pre-ready compose

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import TodoInput from "./components/TodoInput"
+import TodoList from "./components/TodoList"
+
 function App() {
   
   return (
-    <>
-      
-    </>
+    <main>
+      <TodoInput />
+      <TodoList />
+    </main>
   )
 }
 
