a33417a8e270f87eab734d79078607fd3aa09b3f
Sultan Room <sultanroomcode@gmail.com>
2024-05-20 07:57:37 +0700

    fully functional array

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
+import { useState } from "react"
 import TodoInput from "./components/TodoInput"
 import TodoList from "./components/TodoList"
 
 function App() {
-  
+  const [todos, setTodos] = useState([
+    'pergi ke gim',
+    'makan buah',
+    'ksksksal'
+  ])
+
+  const [todoValue, setTodoValue] = useState([])
+
+  function handleAddTodos(newTodo){
+    const newTodoList = [...todos, newTodo];
+    setTodos(newTodoList);
+  }
+
+  function handleUpdateTodos(index)
+  {
+    const valueToBeEdited = todos[index]
+    setTodoValue(valueToBeEdited)
+    handleDeleteTodos(index)
+  }
+
+  function handleDeleteTodos(index)
+  {
+    const newTodoList = todos.filter((todo, todoIndex) => {
+      return todoIndex !== index
+    })
+    setTodos(newTodoList)
+  }
+
   return (
-    <main>
-      <TodoInput />
-      <TodoList />
-    </main>
+    <>
+      <TodoInput todoValue={todoValue} setTodoValue={setTodoValue} handleAddTodos={handleAddTodos} />
+      <TodoList handleUpdateTodos={handleUpdateTodos} handleDeleteTodos={handleDeleteTodos} todos={todos} />
+    </>
   )
 }
 
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import TodoCard from "./TodoCard";
 
-export default function TodoList() {
-  return (
-    <div>
-      
-    </div>
+export default function TodoList(props) {
+    const {todos} = props;
+    
+    return (
+    <ul className="main">
+      {todos.map((todo, todoIndex) => {
+        return (
+            <TodoCard {...props} key={todoIndex} index={todoIndex}>
+                <p>{todo}</p>
+            </TodoCard>
+        )
+      })}
+    </ul>
   )
 }
